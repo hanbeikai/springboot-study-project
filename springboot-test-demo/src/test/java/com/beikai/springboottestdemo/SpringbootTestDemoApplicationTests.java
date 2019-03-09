a054ead1d9c3da231675a9b3250ab228261fb2cd
beikaideyun <hanbeikai@qq.com>
2019-03-09 19:53:30 +0800

    创建 springboot - test-demo 对象

---
+++ springboot-test-demo/src/test/java/com/beikai/springboottestdemo/SpringbootTestDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beikai.springboottestdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootTestDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
