b80e68b79ecd3a91c5bf5a3f57ef89322003db75
beikaideyun <hanbeikai@qq.com>
2019-03-10 09:46:06 +0800

    添加 类的加载机制 测试

---
+++ springboot-test-demo/src/main/java/com/beikai/springboottestdemo/classloadtest/ClassLoadDemo.java
@@ -0,0 +1,67 @@
+package com.beikai.springboottestdemo.classloadtest;
+
+import org.junit.Test;
+
+/**
+ * @ClassName ClassLoadDemo
+ * @Description TODO
+ * @Author Admin
+ * @Date 2019/3/10 9:37
+ * @Version 1.0
+ *
+ * 用于 测试类加载机制
+ *
+ *  结果 :
+ *      加载了静态代码块
+ *      加载了代码块
+ *      加载了构造方法
+ *      调用了静态方法
+ **/
+public class ClassLoadDemo {
+
+    private static String name = "张三";
+
+    private String age;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        ClassLoadDemo.name = name;
+    }
+
+    public ClassLoadDemo(){
+        System.out.println("加载了构造方法");
+    }
+
+    {
+        System.out.println("加载了代码块");
+    }
+
+    static {
+        System.out.println("加载了静态代码块");
+    }
+
+    public static String getThing(){
+        System.out.println("调用了静态方法");
+
+        return getName();
+    }
+
+
+    @Test
+    public void test(){
+        System.out.println(ClassLoadDemo.getThing());
+    }
+
+
+}
