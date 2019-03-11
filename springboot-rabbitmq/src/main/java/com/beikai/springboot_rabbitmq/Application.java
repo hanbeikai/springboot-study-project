98f1b9220c22abfe5733c33aa74371b646595426
beikaideyun <hanbeikai@qq.com>
2019-03-11 23:17:38 +0800

    添加  mq 投递 成功 100% 的方式

--- springboot-rabbitmq/src/main/java/com/beikai/springboot_rabbitmq/Application.java
+++ springboot-rabbitmq/src/main/java/com/beikai/springboot_rabbitmq/Application.java
@@ -3,6 +3,11 @@ package com.beikai.springboot_rabbitmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 保证 mq 投递 100% 可靠的课程
+ * https://www.imooc.com/learn/1042
+ *
+ */
 @SpringBootApplication
 public class Application {
 
