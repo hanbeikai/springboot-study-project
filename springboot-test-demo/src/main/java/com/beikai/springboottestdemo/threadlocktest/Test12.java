32c0f58a223059e8c3cb45f849e4536040eda9bd
beikaideyun <hanbeikai@qq.com>
2019-03-20 07:59:14 +0800

    添加 lock 设置 等待类型

--- springboot-test-demo/src/main/java/com/beikai/springboottestdemo/threadlocktest/Test12.java
+++ springboot-test-demo/src/main/java/com/beikai/springboottestdemo/threadlocktest/Test12.java
@@ -17,14 +17,22 @@ public class Test12 {
 
     volatile int value = 0;
 
+    /**
+     * lock 类里面 被广泛用在了 j.u.c并发里面
+     */
     Lock lock = new ReentrantLock();
 
     /**
      * 调用执行方法
+     *  多个线程调用, 只会有一个线程可以获取到这个锁,其他的都在等待
      */
-    public void add(){
-        // 获取锁
+    public void add() throws InterruptedException {
+        // 获取锁  如果没有获取到 就一直等待
         lock.lock();
+        /*// 获取锁, 如果没有获取到 等待一定时间再获取
+        lock.tryLock(10, TimeUnit.SECONDS);
+        // 获取锁, 只获取一次
+        lock.tryLock();*/
         try {
             value++;
         }finally {
@@ -47,12 +55,17 @@ public class Test12 {
 
             new Thread(() ->{
                 for (int j = 0; j < 10000; j++) {
-                    Test11.add();
-                    System.out.println("当前线程是 : " + Thread.currentThread().getName()+"-"+Test11.value);
-                    if (Test11.value == 10000){
-                        integers.add(Test11.value);
-                        System.out.println("--------------------------------------------------");
+                    try {
+                        Test11.add();
+                        System.out.println("当前线程是 : " + Thread.currentThread().getName()+"-"+Test11.value);
+                        if (Test11.value == 10000){
+                            integers.add(Test11.value);
+                            System.out.println("--------------------------------------------------");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
+
 
                 }
             }).start();
