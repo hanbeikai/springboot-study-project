1258b5e6b9bc12bbb13d5e4ef58f8c86bf20e211
beikaideyun <hanbeikai@qq.com>
2019-03-23 22:41:47 +0800

    springboot-starter  添加一个配置文件类

---
+++ springboot-starter-demo/src/main/java/com/beikai/springbootstarterdemo/config/UserConfiguration.java
@@ -0,0 +1,15 @@
+package com.beikai.springbootstarterdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName UserConfiguration
+ * @Description TODO
+ * @Author Admin
+ * @Date 2019/3/21 21:27
+ * @Version 1.0
+ **/
+@Configuration
+public class UserConfiguration {
+
+}
