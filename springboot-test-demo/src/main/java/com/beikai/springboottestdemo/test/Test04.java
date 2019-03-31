23d9eaa19070c77fff0a9e02463b8f395f2450be
beikaideyun <hanbeikai@qq.com>
2019-03-31 20:44:54 +0800

    修改 添加通过链式编程的方式创建对象的 测试方法

--- springboot-test-demo/src/main/java/com/beikai/springboottestdemo/test/Test04.java
+++ springboot-test-demo/src/main/java/com/beikai/springboottestdemo/test/Test04.java
@@ -1,5 +1,7 @@
 package com.beikai.springboottestdemo.test;
 
+import org.junit.Test;
+
 /**
  * @ClassName Test04
  * @Description TODO
@@ -8,40 +10,30 @@ package com.beikai.springboottestdemo.test;
  * @Version 1.0
  **/
 public class Test04 {
-
     private String name;
     private String age;
     private String username;
-
-
     public String getName() {
         return name;
     }
-
     public Test04 setName(String name) {
         this.name = name;
-
         return this;
     }
-
     public String getAge() {
         return age;
     }
-
     public Test04 setAge(String age) {
         this.age = age;
         return this;
     }
-
     public String getUsername() {
         return username;
     }
-
     public Test04 setUsername(String username) {
         this.username = username;
         return this;
     }
-
     @Override
     public String toString() {
         return "Test04{" +
@@ -50,6 +42,9 @@ public class Test04 {
                 ", username='" + username + '\'' +
                 '}';
     }
-
-
+    @Test
+    public void test02() {
+        Test04 test04 = new Test04().setAge("12").setName("张三").setUsername("zhangsan");
+        System.out.println(test04);
+    }
 }
--- springboot-test-demo/src/main/java/com/beikai/springboottestdemo/test/TestDemo.java
+++ springboot-test-demo/src/main/java/com/beikai/springboottestdemo/test/TestDemo.java
@@ -51,7 +51,4 @@ public class TestDemo {
 
     }
 
-    public static void main(String[] args) {
-    }
-
 }
