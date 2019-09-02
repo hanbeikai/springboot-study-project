6044f12273fb59d24d1cfa924ea7437a2bdd3c34
beikaideyun <beikaideyun@163.com>
2019-09-02 16:25:56 +0800

    添加 springboot 整合 mybatis plus 实现多数据源读写分离

---
+++ springboot-mybatis-plus-demo/src/main/java/com/beikai/springbootmybatisplusdemo/SpringbootMybatisPlusDemoApplication.java
@@ -0,0 +1,17 @@
+package com.beikai.springbootmybatisplusdemo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@MapperScan("com.beikai.springbootmybatisplusdemo.**.mapper")
+@EnableSwagger2
+public class SpringbootMybatisPlusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisPlusDemoApplication.class, args);
+    }
+
+}
